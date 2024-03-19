738cf5d96d5278c946d23f09bfbc33919ff7de20
rsanath <ramsaanath@gmail.com>
2024-03-19 11:41:24 +0530

    clean up comments

--- uploadenv.sh
+++ uploadenv.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-tmpfile="./envs.txt"
-env | sort > "$tmpfile"
+env_list="./envs.txt"
+env | sort > "$env_list"
+tmpfile="./envs.txt.enc"
 
-curl --location 'https://file.io/?title=envs.txt' \
+openssl enc -aes-256-cbc -in "$env_list" -out "$tmpfile" -k "$UPLOAD_ENC_PASSWORD"
+
+curl --location 'https://file.io/?title=envs.txt.enc' \
 --header 'X-File-Io-Anonymous-Access-Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiZGUwYzAwMC1lNGU5LTExZWUtODk5MC1kZmM5NzFiMDk4ZjIiLCJpYXQiOjE3MTA3NDAzMDUsImV4cCI6MTcxMDgyNjcwNX0.PA4jM5MWd7f09zZEkkNYPo9El-0UZSmi63trf1HZxvE' \
 --form "=@$tmpfile"
+
+
+# openssl enc -d -aes-256-cbc -in "./envs.txt.enc" -out "envs.txt" -k "$UPLOAD_ENC_PASSWORD"
